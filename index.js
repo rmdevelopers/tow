7b9aa66cc89bcc1b022c1420f2137418325dc78e
Rafael Mariano <rafaelmp.web@gmail.com>
2019-05-24 16:24:35 -0300

    ytyutyutyut
    tyutyutyu

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ var cron = require('node-cron');
 //const {prefix, token, guildid, channelid } = require('./config.json');
 
 const client = new Discord.Client();
-// client.commands = new Discord.Collection();
-// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// client is an instance of Discord.Client
 
-// for (const file of commandFiles) {
-//     const command = require(`./commands/${file}`);
-    
-// 	client.commands.set(command.name, command);
-// }
+client.login("aFWLF7I4rsXUinW9fykWpxvg3xMIOuZd_UlREg6skalvS4YZMS-YAt8MRAc4-O-v-Geo");
 
+client.on("message", (message) => {
+	message.channel.send("pong!");
+});
 client.once('ready', () => {
 	console.log('Ready!');
 	message.channel.send("pong!"); 
-	client.login('aFWLF7I4rsXUinW9fykWpxvg3xMIOuZd_UlREg6skalvS4YZMS-YAt8MRAc4-O-v-Geo').then(() => {
+	client.login("aFWLF7I4rsXUinW9fykWpxvg3xMIOuZd_UlREg6skalvS4YZMS-YAt8MRAc4-O-v-Geo").then(() => {
         console.log("I am ready");
-        var guild = client.guilds.get('581543474822774784');
-        if(guild && guild.channels.get('581548308372389888')){
-            guild.channels.get('581548308372389888').send("@here Guaxinins invadirão a Guilda em 10 minutos!").then(() => client.destroy());
+        var guild = client.guilds.get("581543474822774784");
+        if(guild && guild.channels.get("581548308372389888")){
+            guild.channels.get("581548308372389888").send("@here Guaxinins invadirão a Guilda em 10 minutos!").then(() => client.destroy());
         } else {
             console.log("nope");
             //if the bot doesn't have guild with the id guildid
