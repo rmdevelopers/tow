6d745a2693aba62d14f5a4bda66c05f19e1cc76b
Rafael Mariano <rafaelmp.web@gmail.com>
2019-05-24 15:57:47 -0300

    teste
    teste

--- events.js
+++ events.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 var cron = require('node-cron');
-const {prefix, token, guildid, channelid } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -17,82 +16,13 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-    if (!client.commands.has(commandName)) return;
-    
-    const command = client.commands.get(commandName);
-
-    try {
-	    command.execute(message, args);
-    } catch (error) {
-	    console.error(error);
-	    message.reply('there was an error trying to execute that command!');
-    }
-});
-
-client.login(token);
-
-// executes every Sunday, Monday, Wednesday, and Friday at 7:20pm
-cron.schedule('20 19 * * 0,1,3,5', function(){
-    client.login(token).then(() => {
-        console.log("I am ready");
-        var guild = client.guilds.get(guildid);
-        if(guild && guild.channels.get(channelid)){
-            guild.channels.get(channelid).send("@here Guild Ball em 10 minutos!").then(() => client.destroy());
-        } else {
-            console.log("nope");
-            //if the bot doesn't have guild with the id guildid
-            // or if the guild doesn't have the channel with id channelid
-        }
-        client.destroy();
-    });
-});
-
 // executes everyday at 6:40pm
-cron.schedule('50 15 * * *', function(){
-    client.login(token).then(() => {
+cron.schedule('58 15 * * *', function(){
+    client.login('aFWLF7I4rsXUinW9fykWpxvg3xMIOuZd_UlREg6skalvS4YZMS-YAt8MRAc4-O-v-Geo').then(() => {
         console.log("I am ready");
-        var guild = client.guilds.get(guildid);
-	guild.channels.get('581548308372389888').send("@here Guaxinins invadirão a Guilda em 10 minutos!").then(() => client.destroy());
-        if(guild && guild.channels.get(channelid)){
-            guild.channels.get(channelid).send("@here Guaxinins invadirão a Guilda em 10 minutos!").then(() => client.destroy());
-        } else {
-            console.log("nope");
-            //if the bot doesn't have guild with the id guildid
-            // or if the guild doesn't have the channel with id channelid
-        }
-        client.destroy();
-    });
-});
-
-// executes every Friday at 7:50pm
-cron.schedule('50 19 * * 5', function(){
-    client.login(token).then(() => {
-        console.log("I am ready");
-        var guild = client.guilds.get(guildid);
-        if(guild && guild.channels.get(channelid)){
-            guild.channels.get(channelid).send("@here Overlord em 10 minutos!").then(() => client.destroy());
-        } else {
-            console.log("nope");
-            //if the bot doesn't have guild with the id guildid
-            // or if the guild doesn't have the channel with id channelid
-        }
-        client.destroy();
-    });
-});
-
-// executes everyday at 8am
-cron.schedule('* 8 * * *', function(){
-    client.login(token).then(() => {
-        console.log("I am ready");
-        var guild = client.guilds.get(guildid);
-        if(guild && guild.channels.get("574045204240269312")){
-            guild.channels.get("574045204240269312").send("Tmat eats cornflakes").then(() => client.destroy());
+        var guild = client.guilds.get('581543474822774784');
+        if(guild && guild.channels.get('581548308372389888')){
+            guild.channels.get('581548308372389888').send("@here Guaxinins invadirão a Guilda em 10 minutos!").then(() => client.destroy());
         } else {
             console.log("nope");
             //if the bot doesn't have guild with the id guildid
